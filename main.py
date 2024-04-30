a8e3ec74a0b0ed4c2bf9638d9987ee27011ed326
karlaSRD <168581367+karlaSRD@users.noreply.github.com>
2024-04-30 12:58:39 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,20 @@ from flask import Flask
 
 app=Flask(__name__)
 
-@app.route("/")
-def Hola_mundo():
-  return "Hola a todos"
+@app.route("/sumar/<int:num1>/<int:num2>")
+def sumar(num1, num2):
+    resultado = num1 + num2
+    return f"Resultado de la suma: {resultado} "
+  
+  @app.route("/restar/<int:num1>/<int:num2>")
+def restar(num1, num2):
+    resultado = num1 - num2
+    return f"Resultado de la resta: {resultado}"
+
+@app.route("/dividir/<int:num1>/<int:num2>")
+def dividir(num1, num2):
+resultado = num1 / num2
+return f"Resultado de la división: {resultado}"
 
 if __name__=="__main__":
   app.run()
